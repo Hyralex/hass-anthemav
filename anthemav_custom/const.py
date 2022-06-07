afc5e640a49581f597307b9cfb6dc04a08bfc2db
Alex Henry <hyralex@gmail.com>
2022-06-07 20:38:34 +1200

    Change domain name to avoid conflict with official anthemav component (#2)
    * Change domain name to avoid conflict with official anthemav component
    * Upgrade to 1.4.0b2

--- anthemav_custom/const.py
+++ anthemav_custom/const.py
@@ -3,6 +3,6 @@ ANTHEMAV_UDATE_SIGNAL = "anthemav_update"
 CONF_MODEL = "model"
 DEFAULT_NAME = "Anthem AV"
 DEFAULT_PORT = 14999
-DOMAIN = "anthemav"
+DOMAIN = "anthemav_custom"
 MANUFACTURER = "Anthem"
 DEVICE_TIMEOUT_SECONDS = 4.0
